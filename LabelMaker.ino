c40a1a9b0e0523d5e77385b6a29f71742a7a90f0
andreisperid <andreisperid@gmail.com>
2021-03-14 14:07:41 -0300

    using accelStepper and movingAvg

--- LabelMaker.ino
+++ LabelMaker.ino
@@ -1,34 +1,28 @@
-#include <Stepper.h>
-//#include <AccelStepper.h>
+#include <AccelStepper.h>
 #include <Servo.h>
+#include <movingAvg.h> 
 
 #define HALFSTEP 8
-
-#define motorPin1  8     // IN1 on ULN2003 ==> Blue   on 28BYJ-48
-#define motorPin2  9     // IN2 on ULN2004 ==> Pink   on 28BYJ-48
-#define motorPin3  10    // IN3 on ULN2003 ==> Yellow on 28BYJ-48
-#define motorPin4  11    // IN4 on ULN2003 ==> Orange on 28BYJ-48
 
 // home sensor
 int sensorPin = A0;
 int emitterPin = A1;
 int sensorState;
+movingAvg sensorStateAvg(200);
 
-// servo and steppers
-const int stepsPerRevolution = 2048;
-Stepper stepperFeed(stepsPerRevolution, 2, 4, 3, 5);
-Stepper stepperChar(stepsPerRevolution, 8, 10, 9, 11);
-//AccelStepper stepperFeed(HALFSTEP, 2, 4, 3, 5);
-//AccelStepper stepperChar(HALFSTEP, 8, 10, 9, 11);
-//int endPoint = 4076;
+// stepper
+const int stepsPerRevolution = 4076;
+AccelStepper stepperFeed(HALFSTEP, 2, 4, 3, 5);
+AccelStepper stepperChar(HALFSTEP, 8, 10, 9, 11);
+int fullCycle;
+float stepsPerChar;
 
-
+// servo
 Servo myServo;
 int restAngle = 20;
 int peakAngle = 64;
-int stepsPerChar;
 
-// chars
+// char
 #define charQuantity 43
 char charSet[charQuantity] = {
   ' ', '-', '.', '2', '3', '4', '5', '6', '7', '8',
@@ -40,7 +34,7 @@ char charSet[charQuantity] = {
 String labelString;
 bool waitingLabel = false;
 
-// homing
+// infrared
 int minLight;
 int maxLight;
 int thresholdLight;
@@ -52,118 +46,117 @@ void setup() {
 	pinMode(sensorPin, INPUT);
 	pinMode(emitterPin, OUTPUT);
 
-
-	/*
 	stepperFeed.setMaxSpeed(1000.0);
 	stepperFeed.setAcceleration(100.0);
 	stepperFeed.setSpeed(200);
 
 	stepperChar.setMaxSpeed(1000.0);
-	stepperChar.setAcceleration(1000.0);
+	stepperChar.setAcceleration(16000.0);
 	stepperChar.setSpeed(50);
-	*/
-	stepperFeed.setSpeed(16);
-	stepperChar.setSpeed(16);
-
-
 
 	myServo.attach(12);
 	myServo.write(restAngle);
 	delay(100);
 
-	stepsPerChar = stepsPerRevolution / charQuantity;
+	Serial.println();
+
+	//stepsPerChar = 100;
+	//stepsPerChar = 95;
+	stepsPerChar = (float)stepsPerRevolution / charQuantity;
+	Serial.print("stepsPerChar: ");
+	Serial.println(stepsPerChar);
+	sensorStateAvg.begin();
 
 	setHome(true);
-	
 }
 
 void test() {
-	//for (int i = 0; i < 30 * charQuantity; i++) {
-		//delay(1000);
-		//stepperChar.step(stepsPerRevolution/charQuantity);
-	/*
-	if (stepperChar.distanceToGo() == 0){
-		stepperChar.setCurrentPosition(0);
-		endPoint = -endPoint;
 
-		stepperChar.moveTo(endPoint);
+	if (stepperChar.distanceToGo() == 0) {
+		stepperChar.setCurrentPosition(0);
+		fullCycle = stepsPerRevolution;
+
+		stepperChar.moveTo(fullCycle);
 	}
 
-		stepperChar.run();*/
-	//}
+	stepperChar.run();
+
 }
 
 void setHome(bool calibrate) {
 	digitalWrite(emitterPin, HIGH);
-
 	if (calibrate) {
+		stepperChar.setCurrentPosition(0);
 		minLight = analogRead(sensorPin);
 		maxLight = analogRead(sensorPin);
 		Serial.println("calibrating...");
-		for (int i = 0; i < stepsPerRevolution; i++) {
-			stepperChar.step(1);
-			sensorState = analogRead(sensorPin);
 
-			if (minLight != 0 && sensorState < minLight) {
-				minLight = sensorState;
+		stepperChar.moveTo(stepsPerRevolution*1.1f);
+		while (stepperChar.distanceToGo() > 0) {
+			sensorState = analogRead(sensorPin);
+			int averaged = sensorStateAvg.reading(sensorState);
+
+			if (minLight != 0 && averaged < minLight) {
+				minLight = averaged;
 			}
-			if (maxLight != 1023 && sensorState > maxLight) {
-				maxLight = sensorState;
+			if (maxLight != 1023 && averaged > maxLight) {
+				maxLight = averaged;
 			}
 			thresholdLight = (minLight + maxLight) / 2;
+
+			//Serial.print("reading (c): ");
+			//Serial.println(sensorState);
+			//Serial.print("avg: ");
+			//Serial.println(averaged);
+
+			stepperChar.run();
 		}
+		//Serial.print("last reading: ");
+		//Serial.println(sensorState);
+		stepperChar.setCurrentPosition(0);
 	}
 
-	Serial.print("calling home...");
-	/*
-	Serial.print(", reading :");
-	Serial.println(sensorState);
-	if (sensorState > thresholdLight) Serial.print("X");
-	Serial.print(" / threshold :");
-	Serial.print(thresholdLight);
-	Serial.print(" / minLight :");
-	Serial.print(minLight);
-	Serial.print(" / maxLight: ");
-	Serial.println(maxLight);
-	*/
+	//Serial.print("threshold :");
+	//Serial.print(thresholdLight);
+	//Serial.print(" << calibrated >> minLight :");
+	//Serial.print(minLight);
+	//Serial.print(" / maxLight: ");
+	//Serial.println(maxLight);
 
-	delay(500);
+	Serial.println("calling home...");
+	delay(200);
+		
+	stepperChar.moveTo(stepsPerRevolution*2);
+	sensorState = analogRead(sensorPin);
 
-	for (int i = 0; i < stepsPerRevolution; i++) {
+	//Serial.print("sensor ");
+	//Serial.println(sensorState);
+	sensorStateAvg.reset();
+	int averaged = sensorStateAvg.reading(sensorState);
+
+	while (averaged < thresholdLight) {
 		sensorState = analogRead(sensorPin);
-
-		/*
-		Serial.print("sample ");
-		Serial.print(i);
-		*/
-
-		//bool backwards;
-
-		if (sensorState < thresholdLight) {
-			//if (currentCharPosition == -1 || currentCharPosition < 22) {
-			stepperChar.step(1);
-			//	backwards = false;
-			//} else if (currentCharPosition > 22) {
-			//	stepperChar.step(-1);
-			//	backwards = true;
-			//}
-		} else {
-			//if (backwards) {
-			//	stepperChar.step(-stepsPerChar);
-			//}
-			currentCharPosition = 22;
-			Serial.print("at home, position ");
-			Serial.println(currentCharPosition);
-			digitalWrite(emitterPin, LOW);
-			break;
-		}
+		averaged = sensorStateAvg.reading(sensorState);
+		stepperChar.run();
+		//if (sensorState > thresholdLight) Serial.println(sensorState);
 	}
+
+	//stepperChar.runToNewPosition(stepsPerChar / 6);
+
+	Serial.print("last reading: ");
+	Serial.println(sensorState);
+	currentCharPosition = 22;
+	stepperChar.setCurrentPosition(0);
+	Serial.print("at home, position ");
+	Serial.println(currentCharPosition);
+
+	digitalWrite(emitterPin, LOW);
 	delay(500);
 }
 
 void feedLabel() {
 	for (int i = 0; i < 50; i++) {
-		stepperFeed.step(-stepsPerRevolution / 7);
+		//stepperFeed.step(-stepsPerRevolution / 7);
 	}
 	delay(200);
 }
@@ -194,16 +187,20 @@ void writeLabel(String label) {
 }
 
 void goToCharacter(char c) {
+
+	int backAdditional = 0;
+
 	for (int i = 0; i < charQuantity; i++) {
 		if (c == charSet[i]) {
 			deltaPosition = i - currentCharPosition + 1;
-			/*
+			
 			if (deltaPosition < 0)
-				deltaPosition += charQuantity;
+				backAdditional = - stepsPerChar / 2;
+			//	deltaPosition += charQuantity;
 			else if (deltaPosition > 0)
-				deltaPosition -= charQuantity;
-			*/
-
+				backAdditional = stepsPerChar / 4;
+			//	deltaPosition -= charQuantity;
+			
 			Serial.print("char ");
 			Serial.print(c);
 			Serial.print(" is on position ");
@@ -216,9 +213,10 @@ void goToCharacter(char c) {
 
 		}
 	}
-	stepperChar.step(stepsPerChar * deltaPosition);
+	//stepperChar.step(stepsPerChar * deltaPosition);
+	stepperChar.runToNewPosition(stepsPerChar * deltaPosition + backAdditional);
 	delay(200);
-	pressLabel();
+	//pressLabel();
 	//feedLabel();
 }
 
@@ -227,11 +225,11 @@ void readSerial() {
 		Serial.println("write label and hit enter");
 		waitingLabel = true;
 	}
-
 	while (Serial.available()) {
 		labelString = Serial.readStringUntil('\n');
 		Serial.print("TAG: ");
 		Serial.println(labelString);
+
 		writeLabel(labelString);
 		waitingLabel = false;
 	}
@@ -239,6 +237,5 @@ void readSerial() {
 
 void loop() {
 	readSerial();
-	//test();
-	
+	//test();	
 }
