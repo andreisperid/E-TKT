987a91e70aa2cc7a076b720d8cd3826e723624ee
andreisperid <andreisperid@gmail.com>
2021-06-05 20:15:58 -0300

    organizing code merge

--- src/LabelMaker.cpp
+++ src/LabelMaker.cpp
@@ -3,11 +3,10 @@
 #include <ESP32Servo.h>
 #include <movingAvg.h>
 // #include <FS.h>
-// #include <ESPAsyncWiFiManager.h>
-
 #include <WiFi.h>
 #include <AsyncTCP.h>
 #include <ESPAsyncWebServer.h>
+#include <ESPAsyncWiFiManager.h>
 #include <DNSServer.h>
 #include "SPIFFS.h"
 
@@ -39,7 +38,7 @@ char charSet[charQuantity] = {
 	'9', '*', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
 	'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
 	'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '+', '+',
-	'+', '+', '+'};
+	'+', '+', '+'};	
 String labelString;
 int charHome = 21;
 bool waitingLabel = false;
@@ -52,30 +51,50 @@ int currentCharPosition = -1;
 int deltaPosition;
 bool reverseCalibration;
 
-//communication -------------------------------------------------------------------
+String labelText;
+bool busyPrinting;
 
+// temporary
+int ledPin = 1;
+String ledState;
+//
+
+
+// communication -------------------------------------------------------------------
+
+// Create AsyncWebServer object on port 80
 AsyncWebServer server(80);
+DNSServer dns;
+
+// Hardcoded login info
 const char *ssid = "Bug";
 const char *password = "temgente";
 const char *PARAM_MESSAGE = "message";
 
-void setHome(bool calibrate) {
+// MECHANICS -------------------------------------------------------------------
+
+void setHome(bool calibrate)
+{
 	digitalWrite(emitterPin, HIGH);
-	if (calibrate) {
+	if (calibrate)
+	{
 		stepperChar.setCurrentPosition(0);
 		minLight = analogRead(sensorPin);
 		maxLight = analogRead(sensorPin);
 		Serial.println("calibrating...");
 
 		stepperChar.moveTo(stepsPerRevolution * 1.1f);
-		while (stepperChar.distanceToGo() > 0) {
+		while (stepperChar.distanceToGo() > 0)
+		{
 			sensorState = analogRead(sensorPin);
 			int averaged = sensorStateAvg.reading(sensorState);
 
-			if (minLight != 0 && averaged < minLight) {
+			if (minLight != 0 && averaged < minLight)
+			{
 				minLight = averaged;
 			}
-			if (maxLight != 1023 && averaged > maxLight) {
+			if (maxLight != 1023 && averaged > maxLight)
+			{
 				maxLight = averaged;
 			}
 			thresholdLight = (minLight + maxLight) / 2;
@@ -104,7 +123,8 @@ void setHome(bool calibrate) {
 
 	int targetPosition = stepsPerRevolution * 2;
 
-	if (reverseCalibration) {
+	if (reverseCalibration)
+	{
 		targetPosition *= -1;
 	}
 
@@ -116,7 +136,8 @@ void setHome(bool calibrate) {
 	sensorStateAvg.reset();
 	int averaged = sensorStateAvg.reading(sensorState);
 
-	while (averaged < thresholdLight) {
+	while (averaged < thresholdLight)
+	{
 		sensorState = analogRead(sensorPin);
 		averaged = sensorStateAvg.reading(sensorState);
 		stepperChar.run();
@@ -127,13 +148,15 @@ void setHome(bool calibrate) {
 	//Serial.print("last reading: ");
 	//Serial.println(sensorState);
 	currentCharPosition = charHome;
-	if (reverseCalibration) {
+	if (reverseCalibration)
+	{
 		//Serial.println("<< reverse");
 		stepperChar.runToNewPosition(-stepsPerChar);
 		stepperChar.setCurrentPosition(0);
 		reverseCalibration = false;
 	}
-	else {
+	else
+	{
 		//Serial.println(">> forward");
 	}
 
@@ -219,11 +242,9 @@ void writeLabel(String label)
 	for (int i = 0; i < label.length(); i++)
 	{
 		goToCharacter(label[i]);
-
 		setHome(false);
 	}
 	goToCharacter('*');
-
 	Serial.println("finished");
 	setHome(false);
 }
@@ -246,15 +267,124 @@ void readSerial()
 	}
 }
 
-// communication
+// COMMUNICATION -------------------------------------------------------------------
 void notFound(AsyncWebServerRequest *request)
 {
 	request->send(404, "text/plain", "Not found");
 }
 
+void initialize() {
+	// Initialize SPIFFS
+	if (!SPIFFS.begin()) {
+		Serial.println("An Error has occurred while mounting SPIFFS");
+		return;
+	}
+
+	// Print ESP32 Local IP Address
+	Serial.println(WiFi.localIP());
+
+	// Route for root / web page
+	server.on("/", HTTP_GET, [](AsyncWebServerRequest* request) {
+		request->send(SPIFFS, "/index.html", String(), false);
+		// request->send(SPIFFS, "/index.html", String(), false, processor);
+	});
+
+	// Route to load style.css file
+	server.on("/style.css", HTTP_GET, [](AsyncWebServerRequest* request) {
+		request->send(SPIFFS, "/style.css", "text/css");
+	});
+
+	// Route to load script.js file
+	server.on("/script.js", HTTP_GET, [](AsyncWebServerRequest* request) {
+		request->send(SPIFFS, "/script.js", "text/javascript");
+	});
+
+	// Route to load fonts
+	server.on("/fontWhite.ttf", HTTP_GET, [](AsyncWebServerRequest* request) {
+		request->send(SPIFFS, "/fontWhite.ttf", "font");
+	});
+	// Route to load fonts
+	server.on("/fontBlack.ttf", HTTP_GET, [](AsyncWebServerRequest* request) {
+		request->send(SPIFFS, "/fontBlack.ttf", "font");
+	});
+
+	// Route to favicon
+	server.on("/favicon.ico", HTTP_GET, [](AsyncWebServerRequest* request) {
+		request->send(SPIFFS, "/favicon.ico"), "image";
+	});
+
+	// Route to set GPIO to HIGH
+	server.on("/tag=", HTTP_GET, [](AsyncWebServerRequest* request) {
+		request->send(SPIFFS, "/index.html", String(), false);
+		// request->send(SPIFFS, "/index.html", String(), false, receiveTag);
+	});
+
+	// Start server
+	server.begin();
+}
+
+void configModeCallback(AsyncWiFiManager* myWiFiManager) {
+	Serial.println("Entered config mode");
+	Serial.println(WiFi.softAPIP());
+	//if you used auto generated SSID, print it
+	Serial.println(myWiFiManager->getConfigPortalSSID());
+}
+
+void wifiManager()
+{
+	//Local intialization. Once its business is done, there is no need to keep it around
+	AsyncWiFiManager wifiManager(&server, &dns);
+	//reset settings - for testing
+	//wifiManager.resetSettings();
+
+	//set callback that gets called when connecting to previous WiFi fails, and enters Access Point mode
+	wifiManager.setAPCallback(configModeCallback);
+
+	//fetches ssid and pass and tries to connect
+	//if it does not connect it starts an access point with the specified name
+	//here  "AutoConnectAP"
+	//and goes into a blocking loop awaiting configuration
+	if (!wifiManager.autoConnect("E-TKT"))
+	{
+		Serial.println("failed to connect and hit timeout");
+		//reset and try again, or maybe put it to deep sleep
+		ESP.restart();
+		// ESP.reset();
+		delay(1000);
+	}
+
+	//if you get here you have connected to the WiFi
+	Serial.println("connected!");
+	initialize();
+}
+
+// DATA
+// Replaces placeholder with LED state value
+String processor(const String& var) {
+	Serial.println(var);
+	if (var == "STATE") {
+		if (digitalRead(ledPin)) {
+			ledState = "ON";
+		} else {
+			ledState = "OFF";
+		}
+		Serial.print(ledState);
+		return ledState;
+	}
+}
+
+String receiveTag(const String& var) {
+	Serial.println("unprocessed tag is: ");
+	Serial.println(var);
+}
+
+
+// CORE
 void setup()
 {
 	Serial.begin(9600);
+	wifiManager();
+
 	pinMode(sensorPin, INPUT);
 	pinMode(emitterPin, OUTPUT);
 
@@ -303,22 +433,18 @@ void setup()
 	// Route for root / web page
 	server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
 			  { request->send(SPIFFS, "/index.html", String(), false); });
-
 	// Route to load style.css file
 	server.on("/style.css", HTTP_GET, [](AsyncWebServerRequest *request)
 			  { request->send(SPIFFS, "/style.css", "text/css"); });
-
 	// Route to load script.js file
 	server.on("/script.js", HTTP_GET, [](AsyncWebServerRequest *request)
 			  { request->send(SPIFFS, "/script.js", "text/javascript"); });
-
 	// Route to load fonts
 	server.on("/fontWhite.ttf", HTTP_GET, [](AsyncWebServerRequest *request)
 			  { request->send(SPIFFS, "/fontWhite.ttf", "font"); });
 	// Route to load fonts
 	server.on("/fontBlack.ttf", HTTP_GET, [](AsyncWebServerRequest *request)
 			  { request->send(SPIFFS, "/fontBlack.ttf", "font"); });
-
 	// Route to favicon
 	server.on("/favicon.ico", HTTP_GET, [](AsyncWebServerRequest *request)
 			  { request->send(SPIFFS, "/favicon.ico"), "image"; });
@@ -326,13 +452,11 @@ void setup()
 	// server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
 	//     request->send(200, "text/plain", "Hello, world");
 	// });
-
 	// // Send a GET request to <IP>/sensor/<number>
 	// server.on("^\\/sensor\\/([0-9]+)$", HTTP_GET, [] (AsyncWebServerRequest *request) {
 	//     String sensorNumber = request->pathArg(0);
 	//     request->send(200, "text/plain", "Hello, sensor: " + sensorNumber);
 	// });
-
 	// // Send a GET request to <IP>/sensor/<number>/action/<action>
 	// server.on("^\\/sensor\\/([0-9]+)\\/action\\/([a-zA-Z0-9]+)$", HTTP_GET, [] (AsyncWebServerRequest *request) {
 	//     String sensorNumber = request->pathArg(0);
@@ -341,7 +465,6 @@ void setup()
 	// });
 
 	server.onNotFound(notFound);
-
 	server.begin();
 }
 
@@ -349,124 +472,3 @@ void loop()
 {
 	// readSerial();
 }
-
-// void configModeCallback(AsyncWiFiManager* myWiFiManager) {
-// 	Serial.println("Entered config mode");
-// 	Serial.println(WiFi.softAPIP());
-// 	//if you used auto generated SSID, print it
-// 	Serial.println(myWiFiManager->getConfigPortalSSID());
-// }
-
-// int ledPin = 1;
-// String ledState;
-
-// //
-// String labelText;
-
-// //
-// bool busyPrinting;
-
-// // Create AsyncWebServer object on port 80
-// AsyncWebServer server(80);
-// DNSServer dns;
-
-// // Replaces placeholder with LED state value
-// String processor(const String& var) {
-// 	Serial.println(var);
-// 	if (var == "STATE") {
-// 		if (digitalRead(ledPin)) {
-// 			ledState = "ON";
-// 		} else {
-// 			ledState = "OFF";
-// 		}
-// 		Serial.print(ledState);
-// 		return ledState;
-// 	}
-// }
-
-// String receiveTag(const String& var) {
-// 	Serial.println("unprocessed tag is: ");
-// 	Serial.println(var);
-// }
-
-// void initialize() {
-// 	// Initialize SPIFFS
-// 	if (!SPIFFS.begin()) {
-// 		Serial.println("An Error has occurred while mounting SPIFFS");
-// 		return;
-// 	}
-
-// 	// Print ESP32 Local IP Address
-// 	Serial.println(WiFi.localIP());
-
-// 	// Route for root / web page
-// 	server.on("/", HTTP_GET, [](AsyncWebServerRequest* request) {
-// 		request->send(SPIFFS, "/index.html", String(), false, processor);
-// 	});
-
-// 	// Route to load style.css file
-// 	server.on("/style.css", HTTP_GET, [](AsyncWebServerRequest* request) {
-// 		request->send(SPIFFS, "/style.css", "text/css");
-// 	});
-
-// 	// Route to load script.js file
-// 	server.on("/script.js", HTTP_GET, [](AsyncWebServerRequest* request) {
-// 		request->send(SPIFFS, "/script.js", "text/javascript");
-// 	});
-
-// 	// Route to load fonts
-// 	server.on("/fontWhite.ttf", HTTP_GET, [](AsyncWebServerRequest* request) {
-// 		request->send(SPIFFS, "/fontWhite.ttf", "font");
-// 	});
-// 	// Route to load fonts
-// 	server.on("/fontBlack.ttf", HTTP_GET, [](AsyncWebServerRequest* request) {
-// 		request->send(SPIFFS, "/fontBlack.ttf", "font");
-// 	});
-
-// 	// Route to favicon
-// 	server.on("/favicon.ico", HTTP_GET, [](AsyncWebServerRequest* request) {
-// 		request->send(SPIFFS, "/favicon.ico"), "image";
-// 	});
-
-// 	// Route to set GPIO to HIGH
-// 	server.on("/tag=", HTTP_GET, [](AsyncWebServerRequest* request) {
-// 		request->send(SPIFFS, "/index.html", String(), false, receiveTag);
-// 	});
-
-// 	// Start server
-// 	server.begin();
-// }
-
-// void wifiManager() {
-// 	//Local intialization. Once its business is done, there is no need to keep it around
-// 	AsyncWiFiManager wifiManager(&server, &dns);
-// 	//reset settings - for testing
-// 	//wifiManager.resetSettings();
-
-// 	//set callback that gets called when connecting to previous WiFi fails, and enters Access Point mode
-// 	wifiManager.setAPCallback(configModeCallback);
-
-// 	//fetches ssid and pass and tries to connect
-// 	//if it does not connect it starts an access point with the specified name
-// 	//here  "AutoConnectAP"
-// 	//and goes into a blocking loop awaiting configuration
-// 	if (!wifiManager.autoConnect("E-TKT")) {
-// 		Serial.println("failed to connect and hit timeout");
-// 		//reset and try again, or maybe put it to deep sleep
-// 		ESP.restart();
-// 		// ESP.reset();
-// 		delay(1000);
-// 	}
-
-// 	//if you get here you have connected to the WiFi
-// 	Serial.println("connected!");
-// 	initialize();
-// }
-
-// void setup() {
-// 	Serial.begin(115200);
-// 	wifiManager();
-// }
-
-// void loop() {
-// }
