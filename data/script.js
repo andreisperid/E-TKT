a39c888cc0bf566f7cd9bbb1bec0ae031ead0c15
andreisperid <andreisperid@gmail.com>
2021-06-06 11:01:39 -0300

    form validation

--- data/script.js
+++ data/script.js
@@ -1,25 +1,44 @@
 function toggleVisibility(x, y) {
-    if (x.style.visibility === "hidden") {
-        x.style.visibility = "visible";
-        y.innerHTML  = "- EXTRAS -";
-    } else if (x.style.visibility === "visible") {
-        x.style.visibility = "hidden";
-        y.innerHTML  = " + [ EXTRAS ] + ";
-    }
+  if (x.style.visibility === "hidden") {
+    x.style.visibility = "visible";
+    y.innerHTML = "- EXTRAS -";
+  } else if (x.style.visibility === "visible") {
+    x.style.visibility = "hidden";
+    y.innerHTML = " + [ EXTRAS ] + ";
+  }
 }
 
-function tagCommand(){
-    window.open("&?tag=" + document.getElementById("text-input").value,"_self")
+function tagCommand() {
+  let fieldValue = document.getElementById("text-input").value;
+
+  if (useRegex(fieldValue)) {    
+    window.open("&?tag=" + fieldValue, "_self");
+
+  } else {
+    document.getElementById("hint").style.color = "red";    
+    document.getElementById("hint").style.textDecoration = "underline";
+    window.alert("allowed: A-Z 0-9 space . - * (20 max)")
+  }
 }
 
-function rwCommand(){
-    window.open("&?rw","_self")
+function clearWarnings(){
+    document.getElementById("hint").style.color = "white";    
+    document.getElementById("hint").style.textDecoration = "none";
 }
 
-function fwCommand(){
-    window.open("&?fw","_self")
+function useRegex(input) {
+    let regex = /^[a-zA-Z0-9 .*]+$/i;
+    return regex.test(input);
 }
 
-function cutCommand(){
-    window.open("&?cut","_self")
+function rwCommand() {
+  window.open("&?rw", "_self");
+}
+
+function fwCommand() {
+  window.open("&?fw", "_self");
+}
+
+function cutCommand() {
+  window.open("&?cut", "_self");
 }
