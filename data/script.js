d9f51fc053ce11828d13a692bf64436470d3f043
andreisperid <andreisperid@gmail.com>
2021-10-11 20:15:52 -0300

    margin options and ui improvement

--- data/script.js
+++ data/script.js
@@ -1,6 +1,7 @@
 document.getElementById("text-input").focus();
 let busy = false;
-let previousInputLength;
+let treatedLabel = "";
+let clear = false;
 
 function toggleVisibility(x, y, openedLabel, closedLabel) {
   if (x.style.visibility === "hidden") {
@@ -16,41 +17,29 @@ function toggleVisibility(x, y, openedLabel, closedLabel) {
 
 function calculateLength() {
   let label = document.getElementById("length-label");
-  let input = document.getElementById("text-input").value;
-
-  label.innerHTML = (input.length < 7 ? 7 : input.length) * 4 + "mm";
+  label.innerHTML =
+    (treatedLabel.length < 7 ? 7 : treatedLabel.length) * 4 + "mm";
 }
 
 function tagCommand() {
   let fieldValue = document.getElementById("text-input").value;
 
   if (useRegex(fieldValue)) {
-    console.log('printing: "' + fieldValue.toLowerCase() + '"');
-
+    console.log('printing: "' + treatedLabel.toLowerCase() + '"');
     document.getElementById("text-input").blur();
     document.getElementById("text-input").disabled = true;
     document.getElementById("clear-button").disabled = true;
     document.getElementById("submit-button").disabled = true;
     var xhr = new XMLHttpRequest();
-    xhr.open("GET", "/&?" + "tag=" + fieldValue.toLowerCase(), true);
+    xhr.open("GET", "/&?" + "tag=" + treatedLabel.toLowerCase(), true);
     xhr.send();
     busy = true;
   }
 }
 
-function formKeyHandler(e) {
-  let keynum;
-  if (window.event) {
-    // IE
-    keynum = e.keyCode;
-  } else if (e.which) {
-    // Netscape/Firefox/Opera
-    keynum = e.which;
-  }
+function drawHelper() {
   let field = document.getElementById("text-input");
   let fieldValue = field.value;
-  let valid;
-
   let space = "X";
   let multiplier = 0;
   let mode = document.getElementById("mode-dropdown").value;
@@ -58,7 +47,6 @@ function formKeyHandler(e) {
     case "margin":
       field.maxLength = 18;
       if (field.value.length > 18) {
-        console.log("slicing string");
         field.value = fieldValue.slice(0, 18);
       }
       multiplier = 1;
@@ -70,40 +58,49 @@ function formKeyHandler(e) {
     case "full":
       multiplier = Math.floor((20 - field.value.length) / 2);
       field.maxLength = 20;
-      console.log(previousInputLength + " / " +  field.value.length)
-      if (
-        previousInputLength !== field.value.length ||
-        previousInputLength == 0
-      ) {
-        let realvalue = field.value.trim();
-        console.log('trimmed: "' + realvalue + '"');
-        field.valid = realvalue;
-
-        if (realvalue.length % 2 != 0) {
-          field.value += " ";
-          field.selectionStart = field.selectionEnd = field.value.length - 1;
-        }
-      }
-      previousInputLength = field.value.length;
-
       break;
   }
 
   document.getElementById("clear-button").disabled = fieldValue === "";
   document.getElementById("submit-button").disabled = fieldValue === "";
 
-  if (fieldValue != "") {
+  if (fieldValue != "" && !clear) {
+    if (multiplier + field.value.length + multiplier < 7) {
+      multiplier = Math.ceil((7 - field.value.length) / 2);
+      console.log("minimum length " + multiplier);
+    }
     document.getElementById("size-helper").innerHTML =
       space.repeat(multiplier) +
-      "o".repeat(field.value.length) +
-      space.repeat(multiplier) +
-      (mode === "full" ? space.repeat((20 - fieldValue.length) % 2) : "");
+      "x".repeat(field.value.length) +
+      space.repeat(multiplier);
+
+    treatedLabel =
+      " ".repeat(multiplier) + field.value + " ".repeat(multiplier);
+    console.log('"' + treatedLabel + '"');
   } else {
+    clear = false;
     document.getElementById("size-helper").innerHTML =
       space.repeat(multiplier) +
       (mode != "full" ? "WRITE HERE" : "") +
       space.repeat(multiplier);
   }
+}
+
+function formKeyHandler(e) {
+  let field = document.getElementById("text-input");
+  let fieldValue = field.value;
+
+  
+  let keynum;
+  if (window.event && e != null) {
+    keynum = e.keyCode;
+  } else if (e.which && e != null) {
+    keynum = e.which;
+  }
+
+  let valid;
+
+  drawHelper();
 
   if (!useRegex(fieldValue) && fieldValue != "") {
     valid = false;
@@ -115,13 +112,13 @@ function formKeyHandler(e) {
     document.getElementById("hint").style.color = "#777777";
     document.getElementById("text-input").style.color = "#ffffff";
   }
-
   if (keynum === 13 && valid) {
     document.getElementById("submit-button").click();
   }
 }
 
 function clearField() {
+  clear = true;
   textField = document.getElementById("text-input");
 
   document.getElementById("clear-button").disabled = true;
@@ -130,6 +127,9 @@ function clearField() {
   document.getElementById("text-input").style.color = "#ffffff";
 
   textField.value = "";
+  treatedLabel = "";
+
+  drawHelper();
   calculateLength();
 
   textField.focus();
@@ -159,18 +159,15 @@ function cutCommand() {
 }
 
 setInterval(function () {
-  // Call a function repetatively with 2 Second interval
   if (busy) {
     getData();
   }
-  // console.log("interval");
-}, 500); //2000mSeconds update rate
+}, 500);
 
 function getData() {
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
-      // document.getElementById("ADCValue").innerHTML = this.responseText;
       console.log(this.responseText);
       document.getElementById("submit-button").value =
         " Printing " + this.responseText + "% ";
@@ -179,7 +176,6 @@ function getData() {
 
       if (this.responseText === "finished") {
         busy = false;
-        // document.getElementById("text-input").blur();
         document.getElementById("progress-bar").style.width = 0;
         document.getElementById("submit-button").value = " Print label! ";
         document.getElementById("text-input").disabled = false;
